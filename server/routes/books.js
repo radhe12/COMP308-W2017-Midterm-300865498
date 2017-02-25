280a35053a4cbceb4d14da06696ca12ecb58e62f
radhe12 <radhey.gor@gmail.com>
2017-02-25 14:27:46 -0500

    Add, update and delete and routes

--- server/routes/books.js
+++ server/routes/books.js
@@ -29,7 +29,19 @@ router.get('/add', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
-
+     game.find( (err, games) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('games/index', {
+        title: 'Games',
+        games: games,
+        displayName: req.user ? req.user.displayName : ''
+      });
+    }
+  });
+ 
 });
 
 // POST process the Book Details page and create a new Book - CREATE
@@ -38,6 +50,20 @@ router.post('/add', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+      let newBook = books({
+      "name": req.body.name,
+      "cost": req.body.cost,
+      "rating": req.body.rating
+    });
+
+    game.create(newBook, (err, books) => {
+      if(err) {
+        console.log(err);
+        res.end(err);
+      } else {
+        res.redirect('/books');
+      }
+    });
 
 });
 
@@ -47,6 +73,28 @@ router.get('/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+      try {
+      // get a reference to the id from the url
+      let id = mongoose.Types.ObjectId.createFromHexString(req.params.id);
+
+        // find one game by its id
+      game.findById(id, (err, games) => {
+        if(err) {
+          console.log(err);
+          res.end(error);
+        } else {
+          // show the game details view
+          res.render('games/details', {
+              title: 'Game Details',
+              games: games,
+              displayName: req.user ? req.user.displayName : ''
+          });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      res.redirect('/errors/404');
+    }
 });
 
 // POST - process the information passed from the details form and update the document
@@ -55,6 +103,24 @@ router.post('/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+     let id = req.params.id;
+
+     let updatedGame = game({
+       "_id": id,
+      "name": req.body.name,
+      "cost": req.body.cost,
+      "rating": req.body.rating
+    });
+
+    game.update({_id: id}, updatedGame, (err) => {
+      if(err) {
+        console.log(err);
+        res.end(err);
+      } else {
+        // refresh the game List
+        res.redirect('/games');
+      }
+    });
 
 });
 
@@ -64,6 +130,17 @@ router.get('/delete/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+      let id = req.params.id;
+
+    game.remove({_id: id}, (err) => {
+      if(err) {
+        console.log(err);
+        res.end(err);
+      } else {
+        // refresh the games list
+        res.redirect('/games');
+      }
+    });
 });
 
 
